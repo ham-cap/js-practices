0f650b5f95a21edb467ca33802a5585f7bcb4a05
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-12-04 17:44:25 +0900

    arrayの作成をベタ打ちから繰り返し処理に変更・再代入しない変数をconstで宣言するよう修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,8 +1,9 @@
-let array = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
+const array = [];
+for (let i = 1; i <= 20; i++) {
+  array.push(i);
+}
 
-for (let number of array) {
+for (const number of array) {
   if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
   } else if (number % 3 === 0) {
