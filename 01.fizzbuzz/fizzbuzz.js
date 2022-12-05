91da83fa7a554debc74a2ccd88d64210579d4673
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-12-05 13:13:41 +0900

    数値をarrayに代入する部分を削除
    ・for文の中で変数iを使って回せばよく、事前に配列を用意する必要がないため削除した。

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,16 +1,11 @@
-const array = [];
 for (let i = 1; i <= 20; i++) {
-  array.push(i);
-}
-
-for (const number of array) {
-  if (number % 3 === 0 && number % 5 === 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (number % 3 === 0) {
+  } else if (i % 3 === 0) {
     console.log("Fizz");
-  } else if (number % 5 === 0) {
+  } else if (i % 5 === 0) {
     console.log("Buzz");
   } else {
-    console.log(number);
+    console.log(i);
   }
 }
