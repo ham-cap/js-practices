cf88d1a9547eba8f2d437eeedea650ff0e964ad4
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-12-10 11:18:40 +0900

    daysOfOneMonthStringの作成段階に行っていた日曜日か否かの判定を出力時のfor文の中に移動した
    ・日曜日の場合のみ、左側に詰める空白の数を減らすために条件を分岐させていたが、桁数を揃えるための空白と日付同士の間隔をあけるための空白を区別することでdaysOfOneMonthStringを作成する際の条件分岐を減らし、見通しを改善した。

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -14,13 +14,9 @@ for (let i = 1; i <= finalDay.day; i++) {
 const daysOfOneMonthString = [];
 for (let i = 0; i < daysOfOneMonth.length; i++) {
   if (daysOfOneMonth[i].weekday === 6) {
-    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(3) + "\n");
-  } else if (daysOfOneMonth[i].weekday !== 7) {
-    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(3));
-  } else if (daysOfOneMonth[i].weekday === 7) {
-    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(2));
+    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(2) + "\n");
   } else {
-    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(3));
+    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(2));
   }
 }
 const SPACES_OF_THE_HEAD = { 1: 5, 2: 8, 3: 11, 4: 14, 5: 17, 6: 20, 7: 2 };
@@ -33,7 +29,10 @@ for (let i = 0; i < daysOfOneMonthString.length; i++) {
         daysOfOneMonthString[i].padStart(SPACES_OF_THE_HEAD[firstDay.weekday])
       )
     );
+  } else if (daysOfOneMonth[i].weekday === 7) {
+    process.stdout.write(String(daysOfOneMonthString[i]));
   } else {
+    process.stdout.write(" ");
     process.stdout.write(String(daysOfOneMonthString[i]));
   }
 }
