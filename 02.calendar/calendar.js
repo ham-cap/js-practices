99f387fa8b2be04ffa904b01da32a3458b933ce8
Toshiyasu Nonohara <hh0648075@gmail.com>
2022-12-10 09:28:37 +0900

    DateTimeオブジェクトからのプロパティの取得方法を変更・日付出力時のバグを修正
    ・DateTimeオブジェクトから月や日などのプロパティを取得する際、get("month")のような書き方だったが、.monthのように書けば取得できるため全てそのように修正した。
    ・日付を順に表示していくfor文で繰返しの回数を誤って一回多く回してしまっていたため末尾にundefinedが出力されてしまっていたバグを修正した。

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,50 +1,42 @@
 const argv = require("minimist")(process.argv);
 const luxon = require("luxon");
 const today = luxon.DateTime.now();
-const month = argv.m || today.get("month");
-const year = argv.y || today.get("year");
+const month = argv.m || today.month;
+const year = argv.y || today.year;
 const date = luxon.DateTime.local(year, month);
 const firstDay = date.startOf("month");
 const finalDay = date.endOf("month");
 const daysOfOneMonth = [];
 
-for (let i = 1; i <= finalDay.get("day"); i++) {
-  daysOfOneMonth.push(
-    luxon.DateTime.local(date.get("year"), date.get("month"), i)
-  );
+for (let i = 1; i <= finalDay.day; i++) {
+  daysOfOneMonth.push(luxon.DateTime.local(date.year, date.month, i));
 }
 const daysOfOneMonthString = [];
 for (let i = 0; i < daysOfOneMonth.length; i++) {
-  if (daysOfOneMonth[i].get("weekday") === 6 && i < 9) {
-    daysOfOneMonthString.push(
-      String(daysOfOneMonth[i].get("day")).padStart(3) + "\n"
-    );
-  } else if (daysOfOneMonth[i].get("weekday") === 6 && i >= 9) {
-    daysOfOneMonthString.push(
-      String(daysOfOneMonth[i].get("day")).padStart(3) + "\n"
-    );
+  if (daysOfOneMonth[i].weekday === 6 && i < 9) {
+    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(3) + "\n");
+  } else if (daysOfOneMonth[i].weekday === 6 && i >= 9) {
+    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(3) + "\n");
   } else if (
     i < 9 &&
-    daysOfOneMonth[i].get("weekday") !== 6 &&
-    daysOfOneMonth[i].get("weekday") !== 7
+    daysOfOneMonth[i].weekday !== 6 &&
+    daysOfOneMonth[i].weekday !== 7
   ) {
-    daysOfOneMonthString.push(String(daysOfOneMonth[i].get("day")).padStart(3));
-  } else if (daysOfOneMonth[i].get("weekday") === 7) {
-    daysOfOneMonthString.push(String(daysOfOneMonth[i].get("day")).padStart(2));
+    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(3));
+  } else if (daysOfOneMonth[i].weekday === 7) {
+    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(2));
   } else {
-    daysOfOneMonthString.push(String(daysOfOneMonth[i].get("day")).padStart(3));
+    daysOfOneMonthString.push(String(daysOfOneMonth[i].day).padStart(3));
   }
 }
 const SPACES_OF_THE_HEAD = { 1: 5, 2: 8, 3: 11, 4: 14, 5: 17, 6: 20, 7: 2 };
-console.log(`      ${date.get("month")}月 ${date.get("year")}`);
+console.log(`      ${date.month}月 ${date.year}`);
 console.log("日 月 火 水 木 金 土");
-for (let i = 0; i <= daysOfOneMonthString.length; i++) {
+for (let i = 0; i < daysOfOneMonthString.length; i++) {
   if (i === 0) {
     process.stdout.write(
       String(
-        daysOfOneMonthString[i].padStart(
-          SPACES_OF_THE_HEAD[firstDay.get("weekday")]
-        )
+        daysOfOneMonthString[i].padStart(SPACES_OF_THE_HEAD[firstDay.weekday])
       )
     );
   } else {
