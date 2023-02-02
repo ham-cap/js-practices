1b27e3445835ae46eb4d512d55e55ac4b3095a00
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-02-02 11:30:09 +0900

    標準入力の受け取り部分を少し修正

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -4,14 +4,35 @@ const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-let lines = [];
-reader.on('line', (line) => {
-    console.log(`Received line is : ${line}`);
+
+ let lines = [];
+ reader.on('line', (line) => {
     lines.push(line);
-    const memo = new Memo(lines)
+    const memo = new Memo(lines);
     console.log(memo.lines)
-});
+ });
+// let lines = [];
+// const memo = reader.on('line', (line) => {
+//   new Memo().lines.push(line)
+// });
 
 // reader.on('close', () => {
-//   console.log(lines);
+//   console.log(memo.constructor.name)
+//   // console.log(lines);
+// });
+
+
+// reader.on('close', () => {
+//   const memo = new Memo()
+//   memo.lines = lines
+//   console.log(memo.lines)
+// });
+
+
+// reader.on('close', () => {
+//   console.log(`これが最終的なlines: ${memo.lines}`);
+// })
+// 
+// reader.on('close', () => {
+//   console.log(memo.history);
 // })
