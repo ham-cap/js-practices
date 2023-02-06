c1ea610849274d00047d8e7b435f66f3434a01a6
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-02-06 09:48:54 +0900

    DBへのレコード保存及び取得のテスト・idをPrimary Keyとして自動で付番するように指定

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -1,16 +1,38 @@
 import {Memo} from './memo.js'
 import * as readline from "readline";
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database('memo_app'); // データベースを作成
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
- let lines = [];
- reader.on('line', (line) => {
+let lines = [];
+reader.on('line', (line) => {
     lines.push(line);
     const memo = new Memo(lines);
-    console.log(memo.lines)
- });
+    // console.log(memo.lines)// linesプロパティにアクセスできているか確認
+    db.serialize(() => {
+      db.run('CREATE TABLE if not exists memos(id INTEGER PRIMARY KEY, body TEXT)');// テーブルの作成
+      db.run('INSERT INTO memos (body) values(?)', [memo.lines]);
+      return db.get('SELECT * FROM memos', function(err, row) {
+        if (err) {
+          throw err;
+        }
+        console.log(row.id)
+        console.log(row.body)
+      });
+    });
+});
+
+// reader.on('close', () => {
+//   console.log(data.body);
+//   console.log(typeof data);
+//   console.log(data.constructor.name);
+// });
+
+
+
 // let lines = [];
 // const memo = reader.on('line', (line) => {
 //   new Memo().lines.push(line)
