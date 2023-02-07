65547c057a22b16e69e801e97aa7a8b1518279e0
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-02-07 14:10:33 +0900

    一覧表示機能（-lオプション）を仮実装

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -2,21 +2,37 @@ import {Memo} from './memo.js'
 import * as readline from "readline";
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database('memo_app'); // データベースを作成
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+// 
+// // メモの保存
+// let lines = [];
+// reader.on('line', (line) => {
+//     lines.push(line);
+//     const memo = new Memo(lines);
+//     db.serialize(() => {
+//       db.run('CREATE TABLE if not exists memos(id INTEGER PRIMARY KEY, body TEXT)');// テーブルの作成
+//       db.run('INSERT INTO memos (body) values(?)', [memo.lines]);
+//     });
+// });
 
-// メモの保存
-let lines = [];
-reader.on('line', (line) => {
-    lines.push(line);
-    const memo = new Memo(lines);
-    db.serialize(() => {
-      db.run('CREATE TABLE if not exists memos(id INTEGER PRIMARY KEY, body TEXT)');// テーブルの作成
-      db.run('INSERT INTO memos (body) values(?)', [memo.lines]);
+if (process.argv[2] == '-l'){
+  db.all('SELECT body FROM memos', function(err, rows) {
+    if (err) {
+      throw err;
+    }
+    rows.forEach(function (row) {
+      const array = row.body.split(',');
+      console.log(array[0]);
     });
-});
+  })
+};
+
+
+
+
 // reader.on('close', () => {});
 // return db.get('SELECT * FROM memos', function(err, row) {
 //   if (err) {
@@ -25,4 +41,4 @@ reader.on('line', (line) => {
 //   console.log(row.id)
 //   console.log(row.body)
 // });
-console.log(process.argv[2]);
+// console.log(process.argv[2]);
