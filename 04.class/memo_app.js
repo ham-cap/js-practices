94d9cc9e177dc41e0103184d98a426195fe9f939
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-02-07 11:33:45 +0900

    メモの保存機能を実装

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -7,54 +7,22 @@ const reader = readline.createInterface({
   output: process.stdout
 });
 
+// メモの保存
 let lines = [];
 reader.on('line', (line) => {
     lines.push(line);
     const memo = new Memo(lines);
-    // console.log(memo.lines)// linesプロパティにアクセスできているか確認
     db.serialize(() => {
       db.run('CREATE TABLE if not exists memos(id INTEGER PRIMARY KEY, body TEXT)');// テーブルの作成
       db.run('INSERT INTO memos (body) values(?)', [memo.lines]);
-      return db.get('SELECT * FROM memos', function(err, row) {
-        if (err) {
-          throw err;
-        }
-        console.log(row.id)
-        console.log(row.body)
-      });
     });
 });
-
-// reader.on('close', () => {
-//   console.log(data.body);
-//   console.log(typeof data);
-//   console.log(data.constructor.name);
+// reader.on('close', () => {});
+// return db.get('SELECT * FROM memos', function(err, row) {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(row.id)
+//   console.log(row.body)
 // });
-
-
-
-// let lines = [];
-// const memo = reader.on('line', (line) => {
-//   new Memo().lines.push(line)
-// });
-
-// reader.on('close', () => {
-//   console.log(memo.constructor.name)
-//   // console.log(lines);
-// });
-
-
-// reader.on('close', () => {
-//   const memo = new Memo()
-//   memo.lines = lines
-//   console.log(memo.lines)
-// });
-
-
-// reader.on('close', () => {
-//   console.log(`これが最終的なlines: ${memo.lines}`);
-// })
-// 
-// reader.on('close', () => {
-//   console.log(memo.history);
-// })
+console.log(process.argv[2]);
