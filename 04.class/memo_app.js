179b3f80c84784ac80badd0f013119c7e493cb2e
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-02-08 15:39:13 +0900

    標準入力からの値の受け取り方法を修正
    ・Memoクラスのインスタンス生成とDBへの保存をどちらもlineイベントのコールバック関数内で行っており、改行のたびにインスタンス生成と保存が実行され、結果として行数と同じ件数のデータが保存されてしまっていたため、それらの処理をcloseイベント内で実行することとした。

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -1,34 +1,39 @@
 import {Memo} from './memo.js'
 import * as readline from "readline";
 import sqlite3 from "sqlite3";
-const db = new sqlite3.Database('memo_app'); // データベースを作成
-// const reader = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-// 
-// // メモの保存
-// let lines = [];
-// reader.on('line', (line) => {
-//     lines.push(line);
-//     const memo = new Memo(lines);
-//     db.serialize(() => {
-//       db.run('CREATE TABLE if not exists memos(id INTEGER PRIMARY KEY, body TEXT)');// テーブルの作成
-//       db.run('INSERT INTO memos (body) values(?)', [memo.lines]);
-//     });
-// });
+const db = new sqlite3.Database('memo_app');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-if (process.argv[2] == '-l'){
-  db.all('SELECT body FROM memos', function(err, rows) {
-    if (err) {
-      throw err;
-    }
-    rows.forEach(function (row) {
-      const array = row.body.split(',');
-      console.log(array[0]);
-    });
-  })
-};
+// 入力の受け取り
+let lines = [];
+reader.on('line', (line) => {
+  lines.push(line);
+});
+
+// Memoインスタンスの生成と保存
+reader.on("close", () => {
+  const memo = new Memo(lines);
+  db.serialize(() => {
+    db.run('CREATE TABLE if not exists memos(id INTEGER PRIMARY KEY, body TEXT)');
+    db.run('INSERT INTO memos (body) values(?)', [memo.lines]);
+  });
+});
+
+// //一覧表示
+// if (process.argv[2] == '-l'){
+//   db.all('SELECT body FROM memos', function(err, rows) {
+//     if (err) {
+//       throw err;
+//     }
+//     rows.forEach(function (row) {
+//       const array = row.body.split(',');
+//       console.log(array[0]);
+//     });
+//   })
+// };
 
 
 
