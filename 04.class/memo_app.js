56df35be1d08ccdfe0bdc0077a6b88eff3fd5fff
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-02-10 11:10:21 +0900

    一覧表示の処理を修正・メモの新規登録処理とオプション使用時の処理への振り分けを仮実装
    ・一覧表示の際、表示が終わっても入力待ちになりCtrl+Cで終了しなければならなかったため、表示が完了した時点でプログラムが終了するようにした。
    ・コマンドの実行時に標準入力とオプションのどちらがあるかで処理を振り分けたかったため、仮で実装した。

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -7,43 +7,40 @@ const reader = readline.createInterface({
   output: process.stdout
 });
 
-// 入力の受け取り
-let lines = [];
-reader.on('line', (line) => {
-  lines.push(line);
-});
-
-// Memoインスタンスの生成と保存
-reader.on("close", () => {
-  const memo = new Memo(lines);
-  db.serialize(() => {
-    db.run('CREATE TABLE if not exists memos(id INTEGER PRIMARY KEY, body TEXT)');
-    db.run('INSERT INTO memos (body) values(?)', [memo.lines]);
+if (process.stdin.isTTY) {
+  //一覧表示
+  if (process.argv[2] == '-l'){
+    db.serialize(() => {
+      db.all('SELECT body FROM memos', function(err, rows) {
+        if (err) {
+          throw err;
+        }
+        rows.forEach(function (row) {
+          const array = row.body.split(',');
+          console.log(array[0]);
+        });
+      })
+      db.close((err) => {
+        if(err){
+          throw err;
+        }
+        db.close();
+      });
+    });
+  };
+} else {
+  // 入力の受け取り
+  let lines = [];
+  reader.on('line', (line) => {
+    lines.push(line);
   });
-});
-
-// //一覧表示
-// if (process.argv[2] == '-l'){
-//   db.all('SELECT body FROM memos', function(err, rows) {
-//     if (err) {
-//       throw err;
-//     }
-//     rows.forEach(function (row) {
-//       const array = row.body.split(',');
-//       console.log(array[0]);
-//     });
-//   })
-// };
-
-
-
-
-// reader.on('close', () => {});
-// return db.get('SELECT * FROM memos', function(err, row) {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log(row.id)
-//   console.log(row.body)
-// });
-// console.log(process.argv[2]);
+  
+  // Memoインスタンスの生成と保存
+  reader.on("close", () => {
+    const memo = new Memo(lines);
+    db.serialize(() => {
+      db.run('CREATE TABLE if not exists memos(id INTEGER PRIMARY KEY, body TEXT)');
+      db.run('INSERT INTO memos (body) values(?)', [memo.lines]);
+    });
+  });
+};
