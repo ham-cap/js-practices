0471623c5ddc7547f2c4b8433f490f07dd4b858c
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-02-15 12:31:19 +0900

    メモの参照機能を実装

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -2,7 +2,7 @@ const Memo = require('./memo.js');
 const readline = require('node:readline/promises');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('memo_app');
-const { Confirm } = require('enquirer');
+const { Select } = require('enquirer');
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -20,16 +20,34 @@ if (process.stdin.isTTY) {
           console.log(array[0]);
           db.close();
         });
-      })
+      });
     });
   }else if (process.argv[2] == '-r'){
-    const prompt = new Confirm({
-      name: 'question',
-      message: 'Did you like enquirer?'
+    db.all('SELECT body FROM memos', function(err, rows) {
+      if (err) {
+        throw err;
+      }
+      const memos = []
+      const titles = []
+      let index = 1
+      rows.forEach(function (row) {
+        const array = row.body.split(',');
+        memos.push(array);
+        titles.push({name: array[0], message: `messageが表示されてる${array[0]}`, value: index});
+        index += 1;
+      });
+      const prompt = new Select({
+        name: 'memos',
+        message: 'Choose a memo.',
+        choices: titles,
+        result() {
+          return this.focused.value;
+        }
+      });
+      prompt.run()
+        .then(answer => memos[answer - 1].forEach(line => console.log(line)))
+        .catch(console.error);
     });
-     
-    prompt.run()
-      .then(answer => console.log('Answer:', answer));
       };
 } else {
   // 入力の受け取り
