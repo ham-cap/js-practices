f9124b79c80cc19f3598d9ab2e9cd72fe33ce033
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-02-21 11:47:19 +0900

    DBからのレコード取得を切り出してメソッド化
    ・最終的にはDBとのやり取りをする部分をクラスとして独立させるため、実験的にメソッド化した。

--- 04.class/db_operator.js
+++ 04.class/db_operator.js
@@ -3,10 +3,18 @@ const db = new sqlite3.Database('memo_app');
 const { Select } = require('enquirer');
 module.exports = class DbOperator {
   displayMemosAndLetUsersChooseOneOfThem(){
-    db.all('SELECT id, body FROM memos', function(err, rows) {
-      if (err) {
-        throw err;
-      }
+    const selectMemosData = function() {
+      return new Promise((resolve, reject) => {
+        db.serialize(() => {
+          db.all('SELECT id, body FROM memos', function(err, rows) {
+            if (err) return reject(err)
+            resolve(rows)
+          })
+        })
+      });
+    }
+    
+    selectMemosData().then(rows => {
       const memos = []
       const titles = []
       rows.forEach(function (row) {
@@ -14,18 +22,26 @@ module.exports = class DbOperator {
         const id = row.id;
         memos.push(body);
         titles.push({name: body[0], message: body[0], value: id});
-      });
-      const prompt = new Select({
-        name: 'memos',
-        message: 'Choose a memo you want to read.',
-        choices: titles,
-        result() {
-          return this.focused.value;
-        }
-      });
-      prompt.run()
-        .then(id => memos[id - 1].forEach(line => console.log(line)))
-        .catch(console.error);
-    });
+      })
+      return [memos, titles]
+    }).then((memos, titles) => {
+      console.log(memos);
+      console.log(titles);
+      })
   };
 };
+
+      //const prompt = new Select({
+      //  name: 'memos',
+      //  message: 'Choose a memo you want to read.',
+      //  choices: titles,
+      //  result() {
+      //    return this.focused.value;
+      //  }
+      //});
+      //prompt.run()
+      //  .then(id => memos[id - 1].forEach(line => console.log(line)))
+      //  .catch(console.error);
+    //});
+    //console.log(memos)
+    //console.log(titles)
